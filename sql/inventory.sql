95c50e4bce2bb2787019bd9359ba3e18d67d7475
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-08-06 22:04:40 +0800

    database

--- sql/inventory.sql
+++ sql/inventory.sql
@@ -1,1 +1,12 @@
 CREATE DATABASE inventory
+
+CREATE TABLE users (
+   user_id int(11) AUTO_INCREMENT PRIMARY KEY not null,
+   user_name VARCHAR(255) NOT NULL,
+   user_email VARCHAR(255) NOT NULL,
+   user_pwd VARCHAR(255) NOT NULL,
+   user_type ENUM('', 'Admin', 'Other') NOT NULL,
+   register_date DATE NOT NULL,
+   last_login DATE NOT NULL,
+   notes VARCHAR(255)
+ );
