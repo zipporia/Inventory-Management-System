9772df3d3e9f21ba4e0a2e707ceee4b65ff87e94
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-08-10 23:09:01 +0800

    Update DBOperation.php
    added category

--- includes/DBOperation.php
+++ includes/DBOperation.php
@@ -10,5 +10,19 @@ class DBOperation{
         $this->conn = $db->connect();
     }
 
+    public function addCategory($parent, $cat){
+        $pre_stmt = $this->conn->prepare("INSERT INTO `categories`(`paretn_cat`, `category_name`, `status`) 
+        VALUES (?,?,?)");
+        $status = 1;
+        $pre_stmt->bind_param("isi", $parent, $cat, $status);
+        $result = $pre_stmt->execute() or die($this->conn->error);
+        if($result){
+            return "CATEGORY_ADDED";
+        }else{
+            return 0;
+        }
+    }// function add category
+} // class DBOperation
 
-}
+//  $opr = new DBOperation();
+//   echo $opr->addCategory(1, "Moblie");
