3b694ccd41ef537a33a5ab29303a1cad8a5efae5
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-08-13 23:49:12 +0800

    Update productModal.php
    Product Modal

--- templates/productModal.php
+++ templates/productModal.php
@@ -9,7 +9,54 @@
                 </button>
             </div>
             <div class="modal-body">
-                ...
+                <form id="product_form" onsubmit="return false">
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                        <label>Date</label>
+                        <input type="text" class="form-control" id="added_date" name="added_date" value="<?php echo date("Y-m-d"); ?>" readonly/>
+                        </div>
+                        <div class="form-group col-md-6">
+                        <label>Product Name</label>
+                        <input type="text" class="form-control" id="product_name" name="product_name" placeholder="Enter Product Name" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputAddress">Address</label>
+                        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                    </div>
+                    <div class="form-group">
+                        <label>Address 2</label>
+                        <select class="form-control" id="select_cat" name="select_cat" required>
+                        
+                        </select>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                        <label for="inputCity">City</label>
+                        <input type="text" class="form-control" id="inputCity">
+                        </div>
+                        <div class="form-group col-md-4">
+                        <label for="inputState">State</label>
+                        <select id="inputState" class="form-control">
+                            <option selected>Choose...</option>
+                            <option>...</option>
+                        </select>
+                        </div>
+                        <div class="form-group col-md-2">
+                        <label for="inputZip">Zip</label>
+                        <input type="text" class="form-control" id="inputZip">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="gridCheck">
+                        <label class="form-check-label" for="gridCheck">
+                            Check me out
+                        </label>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Sign in</button>
+                </form>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
