edb4df27fecd689c30c185e454b8d7b50cbbb0a0
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-08-17 22:53:28 +0800

    Update manage_categories.php

--- manage_categories.php
+++ manage_categories.php
@@ -9,31 +9,30 @@
 <?php include_once 'templates/header.php'?>
     <br>
     <div class="container">
-    <table class="table table-hover">
-    <thead>
-      <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-      </tr>
-    </tbody>
-  </table>
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Category</th>
+                    <th>Parent</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Electronics</td>
+                    <td>Root</td>
+                    <td><a href="#" class="btn btn-success btn-sm">Active</a></td>
+                    <td>
+                        <a href="#" class="btn btn-danger btn-sm">Delete</a>
+                        <a href="#" class="btn btn-info btn-sm">Edit</a>
+                    </td>
+                </tr>
+                
+            </tbody>
+        </table>
     </div>
+
 <?php include_once 'templates/footer.php'?>
