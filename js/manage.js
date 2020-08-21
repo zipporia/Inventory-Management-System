bd3c474fce499bee11e03832d6069605bf1498b3
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-08-21 20:14:57 +0800

    Update manage.js

--- js/manage.js
+++ js/manage.js
@@ -76,4 +76,27 @@ $(document).ready(function(){
         });
     });
 
+    $("#update_category_form").on("submit", function(){
+        if($("#category_name").val() == ""){
+            $("#category_name").addClass("border-danger");
+            $("#cat_error").html("<span class='text-danger'>Please Enter Category Name</span>");
+        }else{
+            $.ajax({
+                url: DOMAIN+"/includes/process.php",
+                method: "POST",
+                data: $("#category_form").serialize(),
+                success: function(data){
+                    if(data == "CATEGORY_ADDED"){
+                        $("#category_name").removeClass("border-danger");
+                        $("#cat_error").html("<span class='text-success'>Category Successfully Added</span>");
+                        $("#category_name").val("");
+                        fetch_category();
+                    }else{
+                        alert(data);
+                    }
+                }
+            });
+        }
+    });
+
 }); // document ready function
