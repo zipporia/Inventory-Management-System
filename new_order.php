0767cbbfb9b67d6c0e3e13f5541eee42c3544c8b
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-08-26 20:03:14 +0800

    Update new_order.php

--- new_order.php
+++ new_order.php
@@ -18,13 +18,13 @@
                     <div class="card-body">
                         <form action="">
                             <div class="form-group row">
-                                <label class="col-sm-3" align="right">Order Date</label>
+                                <label class="col-sm-3 col-form-label" align="right">Order Date</label>
                                 <div class="col-sm-6">
                                     <input type="text" readonly class="form-control form-control-sm" value="<?php echo date("Y-d-m"); ?>">
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label class="col-sm-3" align="right">Customer Name</label>
+                                <label class="col-sm-3 col-form-label" align="right">Customer Name</label>
                                 <div class="col-sm-6">
                                     <input type="text" class="form-control form-control-sm" placeholder="Enter Customer Name" required>
                                 </div>
@@ -47,19 +47,77 @@
                                     <tr>
                                     <td><b id="number">1</b></td>
                                     <td>
-                                        <select name="pid[]" class="form-control form-control-sm">
+                                        <select name="pid[]" class="form-control form-control-sm" required>
                                             <option>Washing Machine</option>
                                         </select>
                                     </td>
-                                    <td> <input name="tqty[]" type="text" class="form-control form-control-sm"> </td>
-                                    <td> <input name="qty[]" type="text" class="form-control form-control-sm"> </td>
+                                    <td> <input name="tqty[]" type="text" class="form-control form-control-sm" readonly> </td>
+                                    <td> <input name="qty[]" type="text" class="form-control form-control-sm" required> </td>
                                     <td> <input name="price[]" type="text" class="form-control form-control-sm" readonly> </td>
                                     <td>Rs.1540</td>
                                     </tr>
                                     </tbody>
                                     </table>
+                                    <center style="padding:10px;">
+                                        <button id="add" style="width:150px;" class="btn btn-success">Add</button>
+                                        <button id="remove" style="width:150px;" class="btn btn-danger">Remove</button>
+                                    </center>
                                 </div>
                             </div>
+                            <p><p>
+                            <div class="form-group row">
+                                <label for="sub_total" class="col-sm-3 col-form-label" align="right">Sub Total</label>
+                                <div class="col-sm-6">
+                                    <input type="text" name="sub_total" id="sub_total" class="form-control form-control-sm">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="gst" class="col-sm-3 col-form-label" align="right">GST (18%)</label>
+                                <div class="col-sm-6">
+                                    <input type="text" name="gst" id="gst" class="form-control form-control-sm">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="discount" class="col-sm-3 col-form-label" align="right">Discount</label>
+                                <div class="col-sm-6">
+                                    <input type="text" name="discount" id="discount" class="form-control form-control-sm">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="net_total" class="col-sm-3 col-form-label" align="right">Net Total</label>
+                                <div class="col-sm-6">
+                                    <input type="text" name="net_total" id="net_total" class="form-control form-control-sm">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="paid" class="col-sm-3 col-form-label" align="right">Paid</label>
+                                <div class="col-sm-6">
+                                    <input type="text" name="paid" id="paid" class="form-control form-control-sm">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="due" class="col-sm-3 col-form-label" align="right">Due</label>
+                                <div class="col-sm-6">
+                                    <input type="text" name="due" id="due" class="form-control form-control-sm">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="payment_type" class="col-sm-3 col-form-label" align="right">Payment Method</label>
+                                <div class="col-sm-6">
+                                    <select name="payment_type" id="payment_type" class="form-control form-control-sm">
+                                        <option value="">Cash</option>
+                                        <option value="">Card</option>
+                                        <option value="">Draft</option>
+                                        <option value="">Cheque</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <center>
+                                <input type="submit" id="order_form" style="width:150px;" class="btn btn-info" value="Order">
+                                <input type="submit" id="print_invoice" style="width:150px;" class="btn btn-success d-none" value="Print Invoice">
+                            </center>
+
                         </form>
                     </div>
                 </div>
