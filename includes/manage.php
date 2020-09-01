f032b9f6e50ef94e7c6125f77043e9e23d217b60
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-09-01 21:52:59 +0800

    Update manage.php

--- includes/manage.php
+++ includes/manage.php
@@ -148,10 +148,16 @@ class Manage
         $pre_stmt->bind_param("ssdddddds", $cust_name, $orderdate, $sub_total, $gst, $discount, $net_total, $paid, $due, $payment_type);
         $pre_stmt->execute() or die($this->conn->error);
         $result = $pre_stmt->get_result();
-        $invoice_no = $result->insert_id
+        $invoice_no = $result->insert_id;
+        if($invoice_no != null){
+            for($i = 0; $i < count($ar_price); $i++){
+                $insert_product = $this->conn->prepare("INSERT INTO `invoice_details`( `invoice_no`, `product_name`, `price`, `qty`) 
+                VALUES (?,?,?,?)");
+                $insert_product->bind_param("isdd", $invoice_no, $ar_pro_name[$i], $ar_price[$i], $ar_qty[$i]);
+                $insert_product->execute() or die($this->conn->error);
+            }
+        }
     }
-
-
 }// Class Manage
 
 //  $obj = new Manage();
