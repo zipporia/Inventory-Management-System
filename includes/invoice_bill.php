e6dcfec89cee56c9ca2f272913adbf96ca4e6325
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-09-03 21:31:10 +0800

    Update invoice_bill.php

--- includes/invoice_bill.php
+++ includes/invoice_bill.php
@@ -7,8 +7,13 @@
     $pdf->AddPage();
 
     $pdf->SetFont("Arial", "B", 16);
-    $pdf->Cell(190,20, "Inventory System", 1, 1, "C");
-    
+    $pdf->Cell(190,20, "Inventory Management System", 0, 1, "C");
+    $pdf->SetFont("Arial", null, 12);
+    $pdf->Cell(40, 10, "Order Date", 0, 0);
+    $pdf->Cell(40, 10, ": 2020-09-03", 0, 1);
+    $pdf->Cell(40, 10, "Customer Name", 0, 0);
+    $pdf->Cell(40, 10, ": Mark Yamar", 0, 1);
+    // $pdf->Cell(190, 10,"", 1, 1);  
 
     $pdf->Output();
   }
