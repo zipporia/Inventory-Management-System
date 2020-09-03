96ef2ef26883889a5406a92335c874cd378fc83a
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-09-04 00:05:38 +0800

    Update invoice_bill.php
    invoice bill complete

--- includes/invoice_bill.php
+++ includes/invoice_bill.php
@@ -27,7 +27,7 @@
       $pdf->Cell(70, 10, $_GET['pro_name'][$i], 1, 0, "C");
       $pdf->Cell(30, 10, $_GET['qty'][$i], 1, 0, "C");
       $pdf->Cell(40, 10, $_GET['price'][$i], 1, 0, "C");
-      $pdf->Cell(40, 10, ($_GET['qty'][$i] * $_GET['price'][$i]) , 1, 0, "C");
+      $pdf->Cell(40, 10, ($_GET['qty'][$i] * $_GET['price'][$i]) , 1, 1, "C");
     }
 
     $pdf->Cell(50, 10,"", 0, 1);
@@ -36,7 +36,18 @@
     $pdf->Cell(50, 10, ": ".$_GET['sub_total'],0,1);
     $pdf->Cell(50, 10, "Gst Tax", 0, 0);
     $pdf->Cell(50, 10, ": ".$_GET['gst'], 0, 1);
+    $pdf->Cell(50, 10, "Discount", 0, 0);
+    $pdf->Cell(50, 10, ": ".$_GET['discount'], 0, 1);
+    $pdf->Cell(50, 10, "Net Total", 0, 0);
+    $pdf->Cell(50, 10, ": ".$_GET['net_total'], 0, 1);
+    $pdf->Cell(50, 10, "Paid", 0, 0);
+    $pdf->Cell(50, 10, ": ".$_GET['paid'], 0, 1);
+    $pdf->Cell(50, 10, "Due Amount", 0, 0);
+    $pdf->Cell(50, 10, ": ".$_GET['due'], 0, 1);
+    $pdf->Cell(50, 10, "Payment Type", 0, 0);
+    $pdf->Cell(50, 10, ": ".$_GET['payment_type'], 0, 1);
 
+    $pdf->Cell(180, 10, "Signature", 0, 0, "R");
 
     $pdf->Output();
   }
